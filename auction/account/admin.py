8eaf2aeca6ae4e68857858d2ce6dc4efaca1ff58
Dhan-Bdr-Karki <dhanbdrkarki111@gmail.com>
2024-02-03 20:15:54 +0545

    custom user model added

--- auction/account/admin.py
+++ auction/account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.db.models import fields
+from django.contrib.auth.admin import UserAdmin
+from account.models import CustomUser
 
-# Register your models here.
+
+class CutomUserAdmin(UserAdmin):
+    list_display = ('email','username','first_name','last_name', 'phone_number','phone_number_confirmation', 'email_confirmation',  'is_admin','is_verified','is_staff','is_active')
+    list_filter = ('email', 'is_admin','is_staff','is_active')
+    ordering = ('-created_at', 'email',)
+    search_fields = ('email','username','first_name','last_name',)
+    fieldsets = (
+        (None, {'fields': ('username','email', 'password',)}),
+        ('Personal Info', {'fields': ('first_name','last_name', 'phone_number',)}),
+        ('Permissions', {'fields': ('is_verified','is_staff', 'is_active', 'is_admin', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','password1', 'password2',),
+            }),
+        ('Personal Info', {'fields': ('first_name','last_name', 'phone_number',)}),
+        ('Permissions', {'fields': ('is_verified','is_staff', 'is_active', 'is_admin', 'is_superuser')}),
+    
+    )
+
+admin.site.register(CustomUser, CutomUserAdmin)
--- auction/account/models.py
+++ auction/account/models.py
@@ -1,4 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from uuid import uuid4
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, phone_number, password):
+        if not email:
+            raise ValueError(_('The email must be set'))
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            phone_number = phone_number,
+        )
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name, phone_number, password):
+        user = self.create_user(
+            email,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+            phone_number = phone_number,
+        )
+        user.is_admin = True
+        user.is_verified = True
+        user.is_staff =True
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    user = models.OneToOneField(User, null=True, blank= True, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50, unique=True, null = True)
+    first_name = models.CharField(_('first name'),max_length=150)
+    last_name = models.CharField(_('last name'),max_length=150)
+
+    email = models.CharField(_('email address'), max_length=150, unique=True)
+    email_confirmation = models.BooleanField(default=False)
+    phone_number = models.CharField(_('phone number'),max_length=20, null = False)
+    phone_number_confirmation = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    is_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager() # setting customer user namanger to the account
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [ 'first_name','last_name', 'phone_number']
+
+    def __str__(self):
+        # TypeError: __str__ returned non-string (type NoneType)
+        if not self.email:
+            return ""
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        if not self.username:
+            # Auto-generate a random username with "user-" prefix
+            self.username = f"user-{str(uuid4())[:8]}"
+        super().save(*args, **kwargs)
+
 
