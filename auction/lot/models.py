5cdfaf72ec9df902503d2dc2bd5f6348b6cd1c29
Aryan Shrestha <aryanshrestha183@gmail.com>
2024-02-05 23:01:13 +0545

    feat(myadmin): category added

--- auction/lot/models.py
+++ auction/lot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.TextField(unique=True, max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, null=True)
+    description = models.CharField(max_length=500, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='sub_category')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+    def __str__(self):
+        return self.name
+    
