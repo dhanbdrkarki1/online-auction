439248b606e80a681906a1d1b2e1d726cdc0dc84
Aryan Shrestha <aryanshrestha183@gmail.com>
2024-02-09 15:28:00 +0545

    category completed

--- auction/myadmin/urls.py
+++ auction/myadmin/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/create/', views.createCategory, name='category_create'),
     path('category/<str:slug>/', views.categoryDetail, name='category_detail'),
+    path('category/delete/<str:slug>/', views.categoryDelete, name='category_delete')
 ]
--- auction/myadmin/views.py
+++ auction/myadmin/views.py
@@ -7,23 +7,36 @@ def index(request):
 
 def createCategory(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        slug = request.POST.get('slug')
-        description = request.POST.get('description')
-        parent_id = request.POST.get('parent')
-
+        action = request.POST.get('action')
         try:
-            if parent_id == None:
-                category = Category.objects.create(name=name, slug=slug, description=description)
-            else:
-                parent = Category.objects.get(id=parent_id)
-                category = Category.objects.create(name=name, slug=slug, description=description, parent=parent)
+            if action == 'create':
+                name = request.POST.get('name')
+                slug = request.POST.get('slug')
+                description = request.POST.get('description')
+                parent_id = request.POST.get('parent_id')
+          
+                if parent_id == "":
+                    category = Category.objects.create(name=name, slug=slug, description=description)
+                else:
+                    parent = Category.objects.get(id=parent_id)
+                    category = Category.objects.create(name=name, slug=slug, description=description, parent=parent)
 
-            messages.success(request, "New category created successfully")
+                messages.success(request, "New category created successfully")
+          
+            else:
+                category_id = request.POST.get('category_id')
+                category = Category.objects.get(id=category_id)
+                category.name = request.POST.get('name')
+                category.slug = request.POST.get('slug')
+                category.description = request.POST.get('description')
+                category.save()
+                messages.success(request,"Category updated")
+
+            return redirect('myadmin:category_detail', category.slug)
+      
         except Exception as e:
-            messages.error(request, e)
-  
-        return redirect('myadmin:category_detail', category.slug)
+                messages.error(request, e)
+                return redirect('myadmin:category_detail')
     
     category_options = Category.objects.all()
 
@@ -32,15 +45,29 @@ def createCategory(request):
         'categories': category_options.filter(parent=None),
     }
 
-    return render(request, "lot/category/create.html", context=context)
+    return render(request, "lot/category/list.html", context=context)
 
 def categoryDetail(request, slug):
     category = Category.objects.get(slug=slug)
 
     context = {
         'category': category,
+        'categories': category.sub_category.all(),
 
     }
-    
-    return render(request, 'lot/category/detail.html', context=context)
+    return render(request, 'lot/category/list.html', context=context)
+
+def categoryDelete(request, slug):
+    category = Category.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        try:
+            category.delete()
+            messages.success(request, f"Category \"{category.name}\" deleted successfully")
+        except Exception as e:
+            messages.error(request, e)
+
+    return redirect('myadmin:category_create')
+
+
 
