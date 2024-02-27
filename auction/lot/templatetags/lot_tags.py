24efbfec3014ee16bba6c2608c6737f9a253167d
Dhan-Bdr-Karki <dhanbdrkarki111@gmail.com>
2024-02-27 17:56:27 +0545

    fix(chat): default image template tag removed

--- auction/lot/templatetags/lot_tags.py
+++ auction/lot/templatetags/lot_tags.py
@@ -10,11 +10,3 @@ register = template.Library()
 @register.filter(name='markdown')
 def markdown_format(text):
  return mark_safe(markdown.markdown(text))
-
-@register.simple_tag
-def image_with_default(image_url):
-    default_image_path = 'client/images/user/user.png'  # Set default image path here
-    if image_url and os.path.exists(os.path.join(settings.MEDIA_ROOT, image_url)):
-        return image_url
-    else:
-        return default_image_path
