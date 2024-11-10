c7a8168b4d710209c41b6f409d750c00cbabd0b3
dhanbdrkarki1 <dhanbdrkarki111@gmail.com>
2024-11-10 20:00:30 +0545

    sequence corrected for using daphne

--- auction/auction/asgi.py
+++ auction/auction/asgi.py
@@ -1,13 +1,22 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auction.settings')
+# Remember to sequence the four key statements in asgi.py while using daphne (even though it works in dev server)
+# from django.core.asgi import get_asgi_application
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# django_asgi_app = get_asgi_application()
+# from chat.routing import websocket_urlpatterns
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+
+import os
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auction.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 import chat.routing
 import lot.routing
-from channels.security.websocket import AllowedHostsOriginValidator
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
